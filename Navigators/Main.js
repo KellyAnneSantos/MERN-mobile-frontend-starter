0995a98c16d0534a9516a2212bed6f43da778e91
Kelly <kelly.a.artola@ricealumni.net>
2023-03-23 23:55:30 -0500

    start authentication

---
+++ Navigators/Main.js
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+const Main = () => {
+  return null;
+};
---
+++ Navigators/UserNavigator.js
@@ -0,0 +1,30 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../Screen/User/Login";
+import Register from "../Screens/User/Register";
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function UserNavigator() {
+  return <MyStack />;
+}
